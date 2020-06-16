8e624af6cfb219574af7a26b2d5689a96210bd83
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-16 18:35:47 +0300

    2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+console.log('true=', 2 + true);
+console.log('false=', 2 + false);
+console.log('\'\'', 2 + '');
+console.log('\'45\'', 2 + '45');
+console.log('45', 2 + 45);
+console.log('undefined', 2 + undefined);
+console.log('null',2 + null);
+console.log('{}',2 + {});
+console.log('Symbol',2 + Symbol);

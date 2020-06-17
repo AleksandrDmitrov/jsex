6cc423e98e632140958a87fcbeecfe25952d01a9
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-17 20:27:17 +0300

    coren

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const x = prompt('пож введите ot 10 do 20');
-const uX = x - 0;
+/**
+ *
+ * @param a
+ * @param b
+ * @param c
+ */
+function sum(a, b, c) {
+    const d = b * b - 4 * a * c;
+    if (d<0) {
+        return null;
+    }
+    const x1 = (-b + d * 0.5) / 2 * a;
+    const x2 = (-b - d * 0.5) / 2 * a;
+    alert('X1= ' + x1);
+    alert('X1= ' + x2);
 
-if (uX >= 10 && uX <= 20) {
-    alert('  TRUE ');
-} else {
-    alert('x = FALSE');
 }
+
+const a = prompt("введите а");
+const b = prompt("введите b");
+const c = prompt("введите c");
+
+sum(a, b, c);
+
+/**
+ *
+ * @param m
+ * @returns {string}
+ */
+function chet(m) {
+    if (m % 2 === 0) {
+        return ("true");
+    } else {
+        return ("false");
+    }
+}
+
+const m = prompt("введите число");
+chet(m);

7326f8eb28c7f56c8b0e47431d8b284cafdb96a6
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-18 19:26:05 +0300

    object exemple3

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const cat = {
-    color: 'red',
-    name: 'vasiliy',
-    breed: 'Siamese',
-    isMale: true,
-    weight: 5,
-    meow : function ()
-    {
-    console.log ( 'MEOW')
+
+
+function  Cat(name, isMale) {
+console.log( this);
 }
+
+const cat1 = new Cat('Murz',true);
+
+const user = {
+    name: 'Test',
+}
+user.age = 55;
+user.speak = function f() {
+alert( 'I am speaking')
 };
-
-
-constcar = {
-    color: 'red',
-    brand: 'WV',
-    model: 'model1',
-    maxSpeed: 220,
-    doorsCount: 5,
-    seatsCount: 5,
-    weight: 2000,
-    engine: {
-        type: 'ICE',
-        volume: 1.6,
-        power: 150,
-        weight: 300,
-    }
-};
-
-alert(constcar.engine.type)
-

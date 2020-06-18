f25655f378d2f6d37c485a3c785828252a1ea9bd
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-18 20:43:24 +0300

    exemple this

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
+function User(firstName, lastName, age) {
+    this.name = firstName;
+    this.surname = lastName;
+    this.age = age;
 
-
-function  Cat(name, isMale) {
-console.log( this);
 }
 
-const cat1 = new Cat('Murz',true);
+const user3 = new User('Pavel', 'Petrov', 464);
+const user4 = new User('Macha', 'Petrova', 25);
+const user5 = new User('Mickel', 'Sidorov', 33);
 
-const user = {
-    name: 'Test',
+function Country(name, population, area) {
+    this.name = name;
+    this.population = population;
+    this.area = area;
+    this.denisty = function denisty() {
+        return (this.population / this.area);
+    };
+
 }
-user.age = 55;
-user.speak = function f() {
-alert( 'I am speaking')
+
+const country1 = new Country('Canada', 150554, 4647);
+const country2 = new Country('Australia', 45554, 85647);
+const country3 = new Country('Ganduras', 2561, 3647);
+
+const USA = {
+    name: 'USA',
+    population: 328000,
+    area: 676687,
+    denisty() {
+        return this.population / this.area;
+
+    },
+};
+
+const car = {
+    speed: 120,
+    acceleration: 20,
+    newSpeed(){
+        return this.speed + this.acceleration;
+    },
 };

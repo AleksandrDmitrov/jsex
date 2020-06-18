466d91707a97036e276c04d058711da9b89cd7bc
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-18 18:38:02 +0300

    object exemple

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
-/**
- *
- * @param a
- * @param b
- * @param c
- * @returns {null|number[]|number}
- */
-function sum(a, b, c) {
-    const d = b * b - 4 * a * c;
-    if (d < 0) {
-        return null;
+const cat = {
+    color: 'red',
+    name: 'vasiliy',
+    breed: 'Siamese',
+    isMale: true,
+    weight: 5,
+};
+
+
+constcar = {
+    color: 'red',
+    brand: 'WV',
+    model: 'model1',
+    maxSpeed: 220,
+    doorsCount: 5,
+    seatsCount: 5,
+    weight: 2000,
+    engine: {
+        type: 'ICE',
+        volume: 1.6,
+        power: 150,
+        weight: 300,
     }
-    if (d === 0) {
-        return -b / (2 * a);
-    }
-    const x1 = (-b + d * 0.5) / 2 * a;
-    const x2 = (-b - d * 0.5) / 2 * a;
-    return [x1, x2];
+};
 
-}
+alert(constcar.engine.type)
 
-const a = prompt("введите а");
-const b = prompt("введите b");
-const c = prompt("введите c");
-
-sum(a, b, c);
-
-/**
- *
- * @param m
- * @returns {string}
- */
-function chet(m) {
-    if (m % 2 === 0) {
-        return ("true");
-    } else {
-        return ("false");
-    }
-}
-
-const m = prompt("введите число");
-chet(m);

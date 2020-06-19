5c99664c9212cf3fc7c59fb75a397ab313291a93
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-19 18:30:33 +0300

    if exemple

--- index.js
+++ index.js
@@ -1,42 +1,17 @@
-function User(firstName, lastName, age) {
-    this.name = firstName;
-    this.surname = lastName;
-    this.age = age;
+const userValue = prompt('Please input number ');
 
+const userNumber = Number(userValue);
+
+if (isNaN(userNumber)) {
+    alert('input NUMBER\! \! \!');
+} else if (userNumber % 5 === 0) {
+    alert('number multiple of 5');
+} else if (userNumber % 3 === 0) {
+    alert('number multiple of 3');
+} else if (userNumber % 2 === 0) {
+    alert('number multiple of 2');
+} else {
+    alert('number not multiple of 5,2,3');
 }
 
-const user3 = new User('Pavel', 'Petrov', 464);
-const user4 = new User('Macha', 'Petrova', 25);
-const user5 = new User('Mickel', 'Sidorov', 33);
 
-function Country(name, population, area) {
-    this.name = name;
-    this.population = population;
-    this.area = area;
-    this.denisty = function denisty() {
-        return (this.population / this.area);
-    };
-
-}
-
-const country1 = new Country('Canada', 150554, 4647);
-const country2 = new Country('Australia', 45554, 85647);
-const country3 = new Country('Ganduras', 2561, 3647);
-
-const USA = {
-    name: 'USA',
-    population: 328000,
-    area: 676687,
-    denisty() {
-        return this.population / this.area;
-
-    },
-};
-
-const car = {
-    speed: 120,
-    acceleration: 20,
-    newSpeed(){
-        return this.speed + this.acceleration;
-    },
-};

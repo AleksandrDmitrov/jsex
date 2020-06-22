bcb095d42689c0a7c7ecf10968e4f20b3fce3a21
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-22 19:59:51 +0300

    while exemple

--- index.js
+++ index.js
@@ -1,51 +1,6 @@
-const firstValue = prompt('Input number 1');
-const firstNumber = Number(firstValue);
-if (!isNaN(firstNumber)) {
-    const secondValue = prompt("Input number 2")
-    const secondNumber = Number(secondValue);
-    if (!isNaN(secondNumber)) {
-        const userFunction = prompt('Choice function +; -; *; /');
-        switch (userFunction) {
-            case '+': {
-                alert('answer = ' + sum(firstNumber, secondNumber))
-            }
-                break;
-            case '-': {
-                alert('answer = ' + subtraction(firstNumber, secondNumber))
-            }
-                break;
-            case '*': {
-                alert('answer = ' + multiplication(firstNumber, secondNumber))
-            }
-                break;
-            case '/': {
-                alert('answer = ' + division(firstNumber, secondNumber))
-            }
-                break;
 
-
-        }
-    }
+let hh = 0;
+while (hh <3) {
+alert( 'Hello\!');
+    hh++;
 }
-
-
-function sum(a, b) {
-    return a + b;
-
-}
-
-function subtraction(a, b) {
-    return a - b;
-
-}
-
-function multiplication(a, b) {
-    return a * b;
-
-}
-
-function division(a, b) {
-    return a / b;
-
-}
-

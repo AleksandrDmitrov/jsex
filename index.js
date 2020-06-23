5046cfa5a920ce2d95706fda2bd0b3805c5e22fc
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-23 18:13:08 +0300

    chablon exemple

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const tempLateStr1 = `2 + 2 = ${2 + 2}`;
 console.log(tempLateStr1);
+
+const tempLateStr2 = `2 - 2 = ${2 - 2}`;
+console.log(tempLateStr2);
+
+const tempLateStr3 = `2 * 2 = ${2 * 2}`;
+console.log(tempLateStr3);
+
+const tempLateStr4 = `2 / 2 = ${2 / 2}`;
+console.log(tempLateStr4);
+
+const firstValue = Number(prompt('Please input you first number'));
+const secondValue = Number(prompt('Please input you second number'));
+alert(`${firstValue} + ${secondValue} = ${firstValue + secondValue}`);

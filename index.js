f1271b9280526b2ee6e503c63bf6e591f234e164
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-23 17:49:14 +0300

    while exemple2

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+// const GOOD_PASSWORD = 'qwerty';
+//
+// let counts = 0
+//
+// while (counts < 5) {
+//     const pass = prompt('enter your password');
+//     if (pass === GOOD_PASSWORD) {
+//         alert('password is correct')
+//         break;
+//     } else {
+//         alert('password is wrong \!\!\!');
+//         counts++
+//     }
+// }
+//  if (counts >= 5) {
+//      alert('Закончились попытки')
+//  }
 
-let hh = 0;
-while (hh <3) {
-alert( 'Hello\!');
-    hh++;
+
+const GOOD_PASSWORD = 'qwerty';
+
+let counts = 0
+let isPasswordEgual = false;
+
+while (counts++ < 5) {
+
+    isPasswordEgual = prompt('enter your password') === GOOD_PASSWORD;
+    if (isPasswordEgual) {
+        break;
+    }
+
 }
+const (`Password $ {isPasswordEgual ? 'Cood' : 'Bad'}`);

c91264358f73ea34ae018d209575eaf011b37eb2
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-23 20:43:01 +0300

    for exemple2

--- index.js
+++ index.js
@@ -15,16 +15,34 @@
 // }
 // console.log(str);
 
-function pow(base, power) {
-    if (power < 0) {
-        return 1 / pow(base,-power);
+// function pow(base, power) {
+//     if (power < 0) {
+//         return 1 / pow(base,-power);
+//     }
+//
+//
+//     let result = 1;
+//     for (let i = 0; i < power; i++){
+//         result *= base;
+//     }
+//
+//   return result;
+// }
+
+function getNthFibNumber(n) {
+    if (n === 1){
+        return 0;
     }
+   let prev1 = 0;
+   let prev2 = 0;
+   let next = prev1 + prev2;
 
+   for (let i=3; i<n; i++){
+       prev1 = prev2;
+       prev2 = next;
+       next = prev1 + prev2;
+   }
 
-    let result = 1;
-    for (let i = 0; i < power; i++){
-        result *= base;
-    }
-
-  return result;
+   return next;
 }
+

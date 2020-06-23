dd93a82109fd9da35a42baafffba06d1ee46c31b
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-23 19:40:18 +0300

    for exemple

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-const CORRECT_PASSWORD = 'qwerty';
-const TRY_LIMIT = 5;
-let tryCount = 0;
 
-while (tryCount++ < TRY_LIMIT) {
-    console.log(`WHILE: ${tryCount}`);
+
+let str = 'Beep!';
+
+
+
+
+
+
+for (let i = 1; i < 10; i++) {
+str += ' Beep!';
+
 }
-
-tryCount = 0;
-
-do {
-    isPasswordCorrect = prompt('Enter password') === CORRECT_PASSWORD;
-    if (isPasswordCorrect)
-        break;
-    {
-    }
-
-} while (++tryCount < TRY_LIMIT);
-
-alert(tryCount >= TRY_LIMIT
-    ? 'Password incorrect'
-    : 'Password correct')
+console.log(str);

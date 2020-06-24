d53aa12de36eb22d5523b761c3f4be6da6d1a8db
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-24 18:00:54 +0300

    for exemple3

--- index.js
+++ index.js
@@ -1,48 +1,6 @@
-//
-//
-// let str = 'Beep!';
-//
-//
-//
-//
-//
-//
-// for (let i = 1; i < 10; i++) {
-// str += ' Beep!';
-// if (i <9){
-//     str += ' '
-// }
-// }
-// console.log(str);
-
-// function pow(base, power) {
-//     if (power < 0) {
-//         return 1 / pow(base,-power);
-//     }
-//
-//
-//     let result = 1;
-//     for (let i = 0; i < power; i++){
-//         result *= base;
-//     }
-//
-//   return result;
-// }
-
-function getNthFibNumber(n) {
-    if (n === 1){
-        return 0;
+const  userValue = Number(prompt('Input your number'));
+if (!isNaN(userValue)){
+    for (let i = 0; i <= 100; i+= userValue){
+        console.log(i);
     }
-   let prev1 = 0;
-   let prev2 = 0;
-   let next = prev1 + prev2;
-
-   for (let i=3; i<n; i++){
-       prev1 = prev2;
-       prev2 = next;
-       next = prev1 + prev2;
-   }
-
-   return next;
 }
-

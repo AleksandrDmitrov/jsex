db88be3283b04176a3a9482d782beb55f89ef4a0
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-24 18:42:37 +0300

    Array exemple familyPopulation [object]

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-//
-// const propName = prompt();
-// const test ={
-//     [propName] : 'Value',
-// };
+const  familyPopulation = Number( prompt('Population'));
 
-const multiplicationTable ={};
-for (let i =2; i < 10; i++){
-    for (let j =2; j < 10; j++){
-multiplicationTable[`${i} * ${j} = `] = i * j;
-    }
+const userFamily = {};
+
+for (let i =0; i < familyPopulation; i++) {
+    userFamily[prompt('Name')] = {
+        age: prompt('Age'),
+        status: prompt('Status'),
+    };
 }
 
-console.log(multiplicationTable);
-console.log(multiplicationTable[`${9} * ${7} = `]);
+console.log( userFamily);

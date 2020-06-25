1b7c1c9e7424bffdfbbd16929a87bbbde532744c
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-25 20:05:26 +0300

    new.target exemple

--- index.js
+++ index.js
@@ -8,5 +8,11 @@ this.name = name;
 
 }
 
+const carProto = new Car();
+carProto.go = function () {
+    alert(`${this.name} car is go`)
+
+}
+
 const car1 = new Car( 'Batmobile');
 const car2 = Car( 'VW');

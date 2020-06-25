127fbef4b879193fae76e53c30c4d1cc64d6b298
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-25 18:03:23 +0300

    this exemple2

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-function MyArray() {
-    this.length = 0;
-    }
-
-    const  myArrayProto = new MyArray();
-
-myArrayProto.puch = function puch(item) {
-this[this.length] = item;
-return ++this.length;
+const duck = {
+    name: 'Duck',
+    fly (){
+        alert(`${this.name} flying !`);
+    },
+    speak (){
+        alert(`Quack -Quack !!!`);
+    },
 };
+const duckChild = Object.create(duck);
 
-MyArray.prototype = myArrayProto;
-const arr = new Array();
-const arr2 = new Array();
-
-const myArray = new MyArray();
-const myArray2 = new MyArray();
-
-
+duckChild.name = 987;

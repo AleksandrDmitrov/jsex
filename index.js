2b947254456c23436eda279cf91dd13f9b030654
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-26 20:44:18 +0300

    filter exemple

--- index.js
+++ index.js
@@ -10,7 +10,6 @@
 // sayHallo( alert, 'Hello my friends');
 
 
-
 // const arr =[1, 2, 3, 4, 5, 6];
 //
 // function alertItem(currentItem) {
@@ -24,30 +23,55 @@
 // });
 
 
-function  User(name, surname, isMale, emailAddress, age, isSubscribed = false) {
-this.firsrName = name;
-this.secondName = surname;
-this.isMale = isMale;
-this.email = emailAddress;
-this.age = age;
-this.isSubscribed = isSubscribed;
+function User(name, surname, isMale, emailAddress, age, isSubscribed = false) {
+    this.firsrName = name;
+    this.secondName = surname;
+    this.isMale = isMale;
+    this.email = emailAddress;
+    this.age = age;
+    this.isSubscribed = isSubscribed;
 }
-const userProto = new User('','',null, '', 0, true);
+
+const userProto = new User('', '', null, '', 0, true);
 userProto.getFullName = function () {
     return `${this.firsrName} ${this.secondName}`;
 };
- User.prototype = userProto;
-  const users =[];
-  for (let i = 0; i < 100; ++i){
-      const newUser = new User(
-          `Username ${i+1}`,
-          `Usersurname ${i+1}`,
-          Math.random() > 0.5,
-          `user.email${i+1}@gmail.com`,
-          Math.ceil( Math.random() * 100),
-          Math.random() > 0.5
-      );
-      users.push(newUser);
-  }
+User.prototype = userProto;
+const users = [];
+for (let i = 0; i < 100; ++i) {
+    const newUser = new User(
+        `Username ${i + 1}`,
+        `Usersurname ${i + 1}`,
+        Math.random() > 0.5,
+        `user.email${i + 1}@gmail.com`,
+        Math.ceil(Math.random() * 100),
+        Math.random() > 0.5
+    );
+    users.push(newUser);
+}
 
-  console.table(users);
+console.table(users);
+
+const usersFullNames = users.map(function (currentUsers) {
+return currentUsers.getFullName();
+});
+console.table(usersFullNames);
+
+
+const getUserIsSubscribed = users.filter(function (item) {
+return item.isSubscribed && !item.isMale && item.age >= 18;
+})
+    .map(function (item) {
+        return item.email;
+
+    });
+console.table(getUserIsSubscribed);
+
+const getUserIsSubscribed2 = users.filter(function (item) {
+    return item.isSubscribed && item.isMale && item.age <= 18;
+})
+    .map(function (item) {
+        return item.email;
+
+    });
+console.table(getUserIsSubscribed2);

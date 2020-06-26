4e74f067651ee5c3bcd7229d86433cafcb18cad8
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-06-26 19:52:56 +0300

    full exemple

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
 'use strict';
+//
+// function sayHallo(executor, ggeeting) {
+//     if (typeof  executor === 'function')
+//         executor( ggeeting);
+//
+// }
+//
+// sayHallo( console.log, 'Hello my friends');
+// sayHallo( alert, 'Hello my friends');
 
-function Car(name) {
-    if (!new.target){
-        return new  Car(name);
-    }
-this.name = name;
 
+
+// const arr =[1, 2, 3, 4, 5, 6];
+//
+// function alertItem(currentItem) {
+//     alert( currentItem);
+// }
+// arr.forEach(alertItem);
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// arr.forEach( function ( item, index) {
+// console.log( `item value ${item},  item index ${index}`);
+// });
+
+
+function  User(name, surname, isMale, emailAddress, age, isSubscribed = false) {
+this.firsrName = name;
+this.secondName = surname;
+this.isMale = isMale;
+this.email = emailAddress;
+this.age = age;
+this.isSubscribed = isSubscribed;
 }
+const userProto = new User('','',null, '', 0, true);
+userProto.getFullName = function () {
+    return `${this.firsrName} ${this.secondName}`;
+};
+ User.prototype = userProto;
+  const users =[];
+  for (let i = 0; i < 100; ++i){
+      const newUser = new User(
+          `Username ${i+1}`,
+          `Usersurname ${i+1}`,
+          Math.random() > 0.5,
+          `user.email${i+1}@gmail.com`,
+          Math.ceil( Math.random() * 100),
+          Math.random() > 0.5
+      );
+      users.push(newUser);
+  }
 
-const carProto = new Car();
-carProto.go = function () {
-    alert(`${this.name} car is go`)
-
-}
-
-const car1 = new Car( 'Batmobile');
-const car2 = Car( 'VW');
+  console.table(users);

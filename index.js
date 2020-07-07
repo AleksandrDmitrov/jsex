f349b1ff8a56c2f9649fcacd8b8b86f936cf44e3
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-07 20:23:10 +0300

    arrow function minimal exemple2

--- index.js
+++ index.js
@@ -28,4 +28,16 @@ const arrowFac = n => n === 0
     ? 1
     : n * arrowFac(n - 1);
 
+const  arr = [1, 2, 3, 4, 5, 6, 7];
+const isOdd = n => n % 2 !== 0;
+const oddDigits = arr.filter(isOdd);
 
+const arrowSum = (...args) => {
+    let total = null;
+    for (let i = 0; i < args.length; i++){
+        total += args[i];
+    }
+return total;
+}
+// создает масив
+console.log(arrowSum(54,4787,45,89));

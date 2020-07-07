5a1f8baee24a1790bacbfbde84519273d8228cd9
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-07 19:34:30 +0300

    arrow function exemple

--- index.js
+++ index.js
@@ -1,77 +1,21 @@
 'use strict';
-//
-// function sayHallo(executor, ggeeting) {
-//     if (typeof  executor === 'function')
-//         executor( ggeeting);
-//
+
+// const sayHalloArrow = () => {
+//     alert('rjhgrejhguijr');
 // }
-//
-// sayHallo( console.log, 'Hello my friends');
-// sayHallo( alert, 'Hello my friends');
+// sayHalloArrow();
 
-
-// const arr =[1, 2, 3, 4, 5, 6];
-//
-// function alertItem(currentItem) {
-//     alert( currentItem);
-// }
-// arr.forEach(alertItem);
-
-// const arr = [1, 2, 3, 4, 5, 6];
-// arr.forEach( function ( item, index) {
-// console.log( `item value ${item},  item index ${index}`);
-// });
-
-
-function User(name, surname, isMale, emailAddress, age, isSubscribed = false) {
-    this.firsrName = name;
-    this.secondName = surname;
-    this.isMale = isMale;
-    this.email = emailAddress;
-    this.age = age;
-    this.isSubscribed = isSubscribed;
+/**
+ *
+ * @param a {number}
+ * @param b{number}
+ * @returns {*} {number}
+ */
+function  sum(a, b) {
+return a + b;
 }
+console.log( sum(555,554) );
 
-const userProto = new User('', '', null, '', 0, true);
-userProto.getFullName = function () {
-    return `${this.firsrName} ${this.secondName}`;
-};
-User.prototype = userProto;
-const users = [];
-for (let i = 0; i < 100; ++i) {
-    const newUser = new User(
-        `Username ${i + 1}`,
-        `Usersurname ${i + 1}`,
-        Math.random() > 0.5,
-        `user.email${i + 1}@gmail.com`,
-        Math.ceil(Math.random() * 100),
-        Math.random() > 0.5
-    );
-    users.push(newUser);
-}
-
-console.table(users);
-
-const usersFullNames = users.map(function (currentUsers) {
-return currentUsers.getFullName();
-});
-console.table(usersFullNames);
-
-
-const getUserIsSubscribed = users.filter(function (item) {
-return item.isSubscribed && !item.isMale && item.age >= 18;
-})
-    .map(function (item) {
-        return item.email;
-
-    });
-console.table(getUserIsSubscribed);
-
-const getUserIsSubscribed2 = users.filter(function (item) {
-    return item.isSubscribed && item.isMale && item.age <= 18;
-})
-    .map(function (item) {
-        return item.email;
-
-    });
-console.table(getUserIsSubscribed2);
+const sumArrow = (a, b) => {
+    return a + b;}
+console.log( sumArrow(555,554) );

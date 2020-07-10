a2842e04f7c1f16b8646daaa457876c687b254a5
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-10 18:33:56 +0300

    class exemple2

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 'use strict';
 
-class User {
-    constructor(name, age) {
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     getFullName() {
+//         return `${this.name} ${this.age}`;
+//     }
+// }
+//
+// const user1 = new User('Petr', '85');
+// const user2 = new User('PAlena', '57');
+// const user3 = new User('Albert', '22');
+
+class Worker {
+    constructor(name, surname, rate = 0, days = 0) {
         this.name = name;
-        this.age = age;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
     }
-    getFullName() {
-        return `${this.name} ${this.age}`;
+    getSalary(){
+        return this.rate * this.days;
     }
 }
 
-const user1 = new User('Petr', '85');
-const user2 = new User('PAlena', '57');
-const user3 = new User('Albert', '22');
+const worker1 = new Worker('Andrey', 'Sidorov', 45, 28);

1edb1e1004c7f3466714a1508e7832148d3b218a
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-10 19:01:17 +0300

    class exemple3

--- index.js
+++ index.js
@@ -14,16 +14,66 @@
 // const user2 = new User('PAlena', '57');
 // const user3 = new User('Albert', '22');
 
-class Worker {
-    constructor(name, surname, rate = 0, days = 0) {
-        this.name = name;
-        this.surname = surname;
-        this.rate = rate;
-        this.days = days;
-    }
-    getSalary(){
-        return this.rate * this.days;
-    }
+
+// class Worker {
+//     constructor(name, surname, rate = 0, days = 0) {
+//         this.name = name;
+//         this.surname = surname;
+//         this.rate = rate;
+//         this.days = days;
+//     }
+//     getSalary(){
+//         return this.rate * this.days;
+//     }
+// }
+//
+// const worker1 = new Worker('Andrey', 'Sidorov', 45, 28);
+
+
+// class User {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//
+//     getFullName() {
+//         return `${this.name} ${this.age}`;
+//     }
+//
+//     isAdult() {
+//         return this.age >= 18;
+//     }
+//
+//
+// }
+//
+// const user1 = new User('Petr', '85');
+// const user2 = new User('PAlena', '15');
+// const user3 = new User('Albert', '22');
+
+class Fuel {
+    constructor(volume, density) {
+        this.volume = volume;
+        this.density = density;
+            }
+            getWeight(){
+        return this.density * this.volume;
+            }
 }
 
-const worker1 = new Worker('Andrey', 'Sidorov', 45, 28);
+
+class Car {
+    /**
+     *
+     * @param {number} volume
+     * @param {Fuel} fuel
+     */
+    constructor(owWeight, fuel) {
+        this.owWeight = owWeight;
+        this.fuel =  fuel;
+    }
+    getFullWeight (){
+        return this.owWeight + this.fuel.getWeight();
+    }
+}
+ const VW = new Car(1400, new Fuel(100, 0.86))

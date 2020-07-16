ebde350e77216b3bc57bce44ed80f146244712e5
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-16 20:13:28 +0300

    Map example2

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 'use strict';
 
-const  myFirstMap = new Map();
+// const  myFirstMap = new Map();
+//
+// myFirstMap.set('key', 'value');
+//
+// const keyValue = myFirstMap.get('key');
+//
+// console.log( keyValue);
 
-myFirstMap.set('key', 'value');
 
-const keyValue = myFirstMap.get('key');
+const vocabulary = new Map();
+vocabulary.set('cat', 'кот');
+vocabulary.set('dog', 'собака');
 
-console.log( keyValue);
+
+function translate(str, separator = ' ') {
+    const englishWords = str.split(separator);
+    const russianWords = englishWords.map(function (word) {
+        return vocabulary.has(word)
+            ? vocabulary.get(word)
+            : word;
+    });
+    return russianWords.join(separator);
+}
+//упрощенная запись
+const translate2 = (str, separator = ' ') => str
+    .split(separator)
+    .map(w => vocabulary.has(w)
+        ? vocabulary.get(w)
+        : w)
+    .join(separator);
+
+alert(translate2('cat'));

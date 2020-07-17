2cc68075e4bca05b4d63133482f8bfea6e03d229
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-17 18:58:45 +0300

    Set example2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const myFirstSet = new Set();
 
-for (let i = 0; i < 100; i++){
+for (let i = 0; i < 20; i++){
     myFirstSet.add( i );
 }
 
@@ -10,3 +10,5 @@ for (let i = 0; i < 100; i++){
 const notUniq = [1,1,1,1,2,2,2,2,3,3,3,3,4,4];
 const setFromArr = new Set(notUniq);
 const Uniq = [...setFromArr];
+
+const mapFromSet = new Map(myFirstSet.entries());

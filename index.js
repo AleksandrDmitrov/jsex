7fa9929b480237a3e1cd04273aec9a392282d09f
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-17 18:42:31 +0300

    Set example

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
 'use strict';
 
-const map = new Map();
+const myFirstSet = new Set();
 
-map.set('key', 'value');
-map.set('key1', 'value1');
-map.set('key2', 'value2');
-map.set('key3', 'value3');
-const iter = map.entries();
-console.log( iter);
+for (let i = 0; i < 100; i++){
+    myFirstSet.add( i );
+}
 
-const map2 = new Map([
-    ['key1', 'value1'],
-    ['key2', 'value2'],
-    ['key3', 'value3'],
-    ['key4', 'value4'],
-    ['key5', 'value5'],
-    ['key6', 'value6'],
-]);
 
-const map2Copy = new Map(map2);
-
-map.forEach((value, key, f) =>{
-    console.log(value,key,f)
-});
+const notUniq = [1,1,1,1,2,2,2,2,3,3,3,3,4,4];
+const setFromArr = new Set(notUniq);
+const Uniq = [...setFromArr];

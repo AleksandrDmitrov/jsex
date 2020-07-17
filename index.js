270563eefb43ef5cb0e5f62b9822a5ffd39018d3
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-17 20:07:01 +0300

    destrukturizachija example

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 'use strict';
 
-const myFirstSet = new Set();
+const user1 = {
+    name: 'Ivan',
+    surname: 'Ivanov',
+    age: 24,
+};
 
-for (let i = 0; i < 20; i++){
-    myFirstSet.add( i );
-}
-
-
-const notUniq = [1,1,1,1,2,2,2,2,3,3,3,3,4,4];
-const setFromArr = new Set(notUniq);
-const Uniq = [...setFromArr];
-
-const mapFromSet = new Map(myFirstSet.entries());
+const { name, surname } = user1;

b43412addbd42f465a97e44838b2001e502d85ea
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-21 20:12:32 +0300

    DOM exemple 2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const buttons = document.getElementsByTagName ('button');
+const btnClickHandler = ({target: 
+    {dataset: {key},
+}
+})=>{
+    console.log(key);
+}
+
+for (const btn of buttons){
+    btn.onclick = btnClickHandler;
+}

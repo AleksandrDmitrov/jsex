8c77a2c4b7f80ebb61b8f74a5556a66a06336e42
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-22 18:58:55 +0300

    сброс дефолтного значения перезагрузки

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 'use strict';
 
-const ul = document.querySelector('ul');
-const li = document.querySelector('li');
-const btn = document.querySelector('button');
-const clickHandler = (e) => {
-    e.stopPropagation();
-    console.dir(e.currentTarget);
-};
-
-btn.addEventListener('click', clickHandler,{
-    once: true,
-});

0f0fb5ae7226e1d678737a29eb3c727b6c17e395
fe2020-1 <dmitrovaleksandr@gmail.com>
2020-07-22 18:49:43 +0300

    perehvat

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const ul = document.querySelector('ul');
 const li = document.querySelector('li');
 const btn = document.querySelector('button');
 const clickHandler = (e) => {
+    e.stopPropagation();
     console.dir(e.currentTarget);
 };
 
-ul.addEventListener('click', clickHandler, true);
-li.addEventListener('click', clickHandler, true);
-btn.addEventListener('click', clickHandler, true);
+btn.addEventListener('click', clickHandler,{
+    once: true,
+});
